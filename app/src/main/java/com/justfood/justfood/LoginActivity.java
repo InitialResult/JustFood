23aa2f85eaf0813f41b825b6c63270e2e8ffe325
Yahye Hussein <yahye.hussein@mail.bcu.ac.uk>
2018-12-26 17:51:23 +0000

    done some of the activity needs to finish the rest

--- app/src/main/java/com/justfood/justfood/LoginActivity.java
+++ app/src/main/java/com/justfood/justfood/LoginActivity.java
@@ -1,20 +1,28 @@
 package com.justfood.justfood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
 public class LoginActivity extends AppCompatActivity {
     private EditText country_code;
     private EditText phone_number;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getSupportActionBar().hide();
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.country_no, android.R.layout.simple_spinner_item);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -25,5 +33,35 @@ public class LoginActivity extends AppCompatActivity {
             country_code = findViewById(R.id.country_code);
             country_code.setText("44");
         }
+        if (getIntent().hasExtra("phone number")) {
+            String getUserPhoneNumber = getIntent().getStringExtra("phone number").substring(4);
+            phone_number.setText(getUserPhoneNumber);
+        }
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+            }
+        };
+    }
+
+    public void onClick(View view) {
+//        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+//                country_code.getText().toString()+phone_number.getText().toString(),        // Phone number to verify
+//                60,                 // Timeout duration
+//                TimeUnit.SECONDS,   // Unit of timeout
+//                this,               // Activity (for callback binding)
+//                mCallbacks);        // OnVerificationStateChangedCallbacks
+
+
+        String code_phone_number = country_code.getText().toString() + " " + phone_number.getText().toString();
+        Intent intent = new Intent(this, VerifyActivity.class);
+        intent.putExtra("phone number", code_phone_number);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/justfood/justfood/MainActivity.java
@@ -0,0 +1,45 @@
+package com.justfood.justfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        textView = findViewById(R.id.test);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.social:
+                break;
+            case R.id.barcode:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
